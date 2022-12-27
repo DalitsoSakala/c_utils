ba5c08342f1378ef3acb5cb74fe7cf50040e7a85
dalitso.sakala <dalitso.1sc@gmail.com>
2022-12-27 17:35:38 +0200

    Added `ulong_t` type

--- include/modular.h
+++ include/modular.h
@@ -1,38 +1,40 @@
 #define ERRNOT_INVERTIBLE 2
 #define ERRPQ_COPRIME 3
 
+typedef unsigned long long int ulong_t;
+
 // RSA Encryption integers
 struct RSACfg {
-	long d;
-	long e;
-	long p;
-	long q;
+  ulong_t d;
+  ulong_t e;
+  ulong_t p;
+  ulong_t q;
 };
 
 // Generates d by iteration
-int make_d(long *d, const struct RSACfg* cfg);
+int make_d(ulong_t *d, const struct RSACfg *cfg);
 
+ulong_t mod_pow(ulong_t a, ulong_t exp, ulong_t mod);
 
 // Returns the GCD of the arguments
-long gcd(long, long);
+ulong_t gcd(ulong_t, ulong_t);
 
-// Calculates modular inverse of a under 
+// Calculates modular inverse of a under
 // modulus n assigning the inverse to result.
 // Returns 2 if a is not invertible and 0
 // on success
-int mod_inv(long*result,const long a,const long n);
+int mod_inv(ulong_t *result, const ulong_t a, const ulong_t n);
 
 // Encrypts the second argument assigning result
 // to the first argument using RSAConfig.
-int encrypt(long*, const long, const struct RSACfg*);
+int encrypt(ulong_t *, const ulong_t, const struct RSACfg *);
 
 // Encrypts the second argument assigning result
 // to the first argument using RSAConfig.
 // Returns error
-int decrypt(long*, const long, const struct RSACfg*);
+int decrypt(ulong_t *, const ulong_t, const struct RSACfg *);
 
-
-// Returns 0 when c is invalid 
-// that is (GCD of p, q and e is not 1) or 1 if 
+// Returns 0 when c is invalid
+// that is (GCD of p, q and e is not 1) or 1 if
 // GCD is valid.
-int valid_rsa(const struct RSACfg* c);
+int valid_rsa(const struct RSACfg *c);
